beaa5dc827cef1bfbb5dd877ca47faab66650cf2
a.galeev <a.galeev@malltech.ru>
2023-12-02 01:23:00 +0500

    markup note

---
+++ src/components/Note.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="note">
+    <p class="note__title">{{ dataNote.title }}</p>
+    <p class="note__text">{{ dataNote.text }}</p>
+    <div class="note__remove">
+      <img src="/images/close.svg" alt="close" />
+      Удалить
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataNote: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
